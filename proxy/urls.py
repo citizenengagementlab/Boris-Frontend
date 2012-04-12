a60626e2292ee315312907b9c748667ef1a7a0b0
Josh Levinger <josh.l@engagementlab.org>
2012-04-12 15:12:12 -0700

    move register to /

--- proxy/urls.py
+++ proxy/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 from django.conf import settings
 
 urlpatterns = patterns('proxy.views',
-    (r'^register/$', 'register'),
+    (r'^$', 'register'),
     (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT }),
     (r'^proxy/(?P<url>.*)$', 'proxy'),
 )
