94fa1a4994f77628dd1834a021537b3262b175a8
Josh Levinger <josh.l@engagementlab.org>
2012-04-11 17:13:56 -0700

    registration initiial commit

--- scripts/js/scripts.js
+++ scripts/js/scripts.js
@@ -2,30 +2,43 @@ var hash = document.location.hash;
 			
 // Sets up the three UI templates.  Changing the hash tag in the URL switches the UI.
 
+(function( $ ){
+	$.fn.serializeJSON=function() {
+		var json = {};
+		jQuery.map($(this).serializeArray(), function(n, i){
+			json[n['name']] = n['value'];
+		});
+		return json;
+	};
+})( jQuery );
+
 $(document).ready(function() {
-if (hash === "#tab") {
-	$('.tabs').tab('show');
-	$('.tabs').show();
-	$("form").addClass("tab-content");
-	$("#tab-new, #tab-personal , #tab-additional, #tab-action").addClass("tab-pane");
-	$("h1").removeAttr("data-toggle");
-	$("#accordion-new, #accordion-personal, #accordion-additional, #accordion-action").removeClass("collapse");
-	$("button.continue").removeAttr("data-toggle");
-} else if (hash === "#accordion") {
-	$('.tabs').hide();
-	$("#tab-link").removeAttr("data-toggle");
+	//ui switch functionality
+	if (hash === "#tab") {
+		$('.tabs').tab('show');
+		$('.tabs').show();
+		$("form").addClass("tab-content");
+		$("#tab-new, #tab-personal , #tab-additional, #tab-action").addClass("tab-pane");
+		$("h1").removeAttr("data-toggle");
+		$("#accordion-new, #accordion-personal, #accordion-additional, #accordion-action").removeClass("collapse");
+		$("button.continue").removeAttr("data-toggle");
+	} else if (hash === "#accordion") {
+		$('.tabs').hide();
+		$("#tab-link").removeAttr("data-toggle");
 	
-} else {
-	$('.tabs').hide();
-	$("h1").removeAttr("data-toggle");
-	$("#accordion-new, #accordion-personal, #accordion-additional, #accordion-action").removeClass("collapse");
-	$("button.continue").hide();
+	} else {
+		$('.tabs').hide();
+		$("h1").removeAttr("data-toggle");
+		$("#accordion-new, #accordion-personal, #accordion-additional, #accordion-action").removeClass("collapse");
+		$("button.continue").hide();
 	}
 	
-	$(".mailing").css("display","none");
-	$(".name-change").css("display","none");
-	$(".address-change").css("display","none");
+	//hide optional fields
+	$(".mailing").hide();
+	$(".name-change").hide();
+	$(".address-change").hide();
 	
+	//show optional fields when necessary
 	$("#has_mailing_address").click(function() {
 		if($("#has_mailing_address").is(":checked")) {
 			$('.mailing').slideDown();
@@ -33,7 +46,6 @@ if (hash === "#tab") {
 			$('.mailing').slideUp();
 		}
 	});
-	
 	$("#change_of_name").click(function() {
 		if($("#change_of_name").is(":checked")) {
 			$('.name-change').slideDown();
@@ -41,7 +53,6 @@ if (hash === "#tab") {
 			$('.name-change').slideUp();
 		}
 	});
-	
 	$("#change_of_address").click(function() {
 		if($("#change_of_address").is(":checked")) {
 			$('.address-change').slideDown();
@@ -66,6 +77,7 @@ if (hash === "#tab") {
 					var party = response.party_list[i];
 					$('select#party').append("<option val="+party+">"+party+"</option>");
 				}
+				
 				//requirements
 				
 				//id validation
@@ -77,25 +89,33 @@ if (hash === "#tab") {
 		});
 });
 	
+	//second api hit, send off registration
+	$("button#register").click(function(event) {
+		event.preventDefault();
+		form_data = $('form#ovr').serializeJSON();
+		//add data that doesn't yet have inputs
+		form_data.lang='en';
+		form_data.partner_id='1';
+		form_data.partner_tracking_id='0';
+		$.ajax({
+			type:"post",
+			url:"https://rtvdemo:bullwinkle@rtvstaging2.osuosl.org/api/v1/registrations.json",
+			data:form_data,
+			dataType:'jsonp',
+			success:function(response) {
+				console.log(response);
+			},
+			complete:function() {
+				console.log('complete');
+			}
+		});
 	});
-});
-/*
-$("#accordion-group").live("click", function() {
-				var url="https://rtvstaging2.osuosl.org/api/v1/registrations.json?callback=?";
-				$.getJSON(url, function(data) {
-					alert(data);
-				});
-			});
-*/
 
 
-	$(".tab-link").click(function() {
+/*	$(".tab-link").click(function() {
 		var link = $(this).attr("href");
 		$("a[href=" + link +"]").tab('show');
-	});
-		
-	/* Don't delete this */
-	});
+	});*/
 
-
-//tooltips
+	//end document(ready)
+});
