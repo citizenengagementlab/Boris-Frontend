b61a293768e6a3206621521530c67ac6bc81d349
Josh Levinger <josh.l@engagementlab.org>
2012-04-13 16:00:49 -0700

    debug print statements

--- proxy/views.py
+++ proxy/views.py
@@ -5,14 +5,14 @@ from django.shortcuts import render_to_response
 from django.conf import settings
 PROXY_FORMAT = u"https://%s/%s" % (settings.PROXY_DOMAIN, u"%s")
 
-    print "PROXY",request.method,
 def rtv-proxy(request, url):
+    if settings.DEBUG: print "PROXY",request.method,
     if request.method == "GET":
-        print request.GET
+        if settings.DEBUG: print request.GET
         url_ending = "%s?%s" % (url, urllib.urlencode(request.GET))
         url = PROXY_FORMAT % url_ending
     elif request.method == "POST":
-        print request.POST
+        if settings.DEBUG: print request.POST
         url = PROXY_FORMAT % url
         #don't use urllib.urlencode to encode data dictionary,
         #that munges the brackets, do &-join manually and quote_plus the values
@@ -20,7 +20,7 @@ def rtv-proxy(request, url):
         for (k,v) in request.POST.items():
             data.append('%s=%s' % (k,urllib.quote_plus(v)))
         data = "&".join(data)
-        print "POST QUERY",data
+        if settings.DEBUG: print "POST QUERY",data
     
     if settings.PROXY_CREDENTIALS:
         #setup a password manager to handle the authentication
