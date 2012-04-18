03d1ac18fc26bbc5fd369bdd86cc14f89b092fc1
Mike Vattuone <mvattuone@gmail.com>
2012-04-18 16:14:31 -0700

    rough accordion
    still working out css + bugs, but it's functional

--- boris/media/js/accordion.js
+++ boris/media/js/accordion.js
@@ -1,1 +1,28 @@
 // TODO - Accordion Form
+
+$(function() 
+
+{
+	$("input#get_started").click(function() {
+	
+		$(".accordion-content:not(:first)").hide();
+		$(".accordion-header").click(function() {
+			$(".accordion-content").slideUp("slow");
+			$(this).next().slideDown("slow");
+		});
+		$(".accordion-next").click(function() {
+			$(this).parent().parent().parent(this).children(".accordion-content").slideUp();
+			console.log($(this).parent().parent().parent(this).next().children().slideDown());
+		});
+		
+		
+		
+	});
+	
+
+		
+
+	//when user clicks header, hides all other sections but one associated with that header (accordion-header)
+
+});
+	
