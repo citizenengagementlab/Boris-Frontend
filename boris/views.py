0a67cffd96610c49d006634375177d40d0fd2c89
Josh Levinger <josh.l@engagementlab.org>
2012-04-19 16:12:30 -0700

    remove js hash, do view switching by get parameter

--- boris/views.py
+++ boris/views.py
@@ -5,7 +5,10 @@ def frontpage(request):
     return redirect('/registrants/new/')
 
 def register(request):
-    return render_to_response('form.html',context_instance=RequestContext(request))
+    layout = request.GET.get('layout')
+    context = {'layout':layout}
+    return render_to_response('form.html',context,
+                context_instance=RequestContext(request))
     
 def finish(request):
     return render_to_response('finish.html',context_instance=RequestContext(request))
