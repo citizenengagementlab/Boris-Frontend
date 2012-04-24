aa0f42b4ca415fe0daba8c171317b04071c2eb34
Mike Vattuone <mvattuone@gmail.com>
2012-04-23 19:38:52 -0700

    Working on getting tabs fixed.

--- boris/media/js/tabs.js
+++ boris/media/js/tabs.js
@@ -1,53 +1,59 @@
 function tabs() {
-	
 	$("document").ready(function() {
-		$(".accordion-content:not(:first)").hide();
+	
+		var buttons = '<input type="button" class="next-button" value="Continue" /><input type="button" data-tab="" class="prev-button" value="Go Back" />';
+		//Hide all tabs except the first
+		$(".accordion:not(:first)").hide();
+		//Show the tab navigation 
 		$("ul#tabnav").show();
 		//show the first step as being active when the page loads
 		$("ul#tabnav li a:first").addClass("tab-active");
 		//hide accordion legends
 		$(".accordion-header").hide();
-		//show continue and go back buttons
-		$(".next-button, .prev-button").show();
+		//html to be injected into each tab section
 		
-		$("ul#tabnav li a").click(function() {	
-			$(this).addClass("tab-active");
-			$("ul#tabnav li a").not($(this)).removeClass("tab-active");
-			$(".accordion-content:visible").hide();
-			$(".accordion-content[data-content=" + $(this).attr("data-tab") + "]").show();
-		});
-		
-		$("input#get_started").click(function() {
-			if(submitStartForm()) {
-			//hide the current content
-			$(this).parent().parent(this).hide();
-			//show the next data-tab
-			console.log($(".accordion-content[data-content=" + $(this).attr("data-tab") + "]").show());
-			}
-		});
+
+		//TODO: loop through each fieldset, append buttons to the bottom
+		$("fieldset:not(:first)").each(function () {
+			$(this).append(buttons);
+		});		
 	
-		$(".next-button, input#get_started").click(function() {
+		//TODO: How do I mix AJAX in with this
+		$("input#get_started").click(function() {
+			//Hide get_started fieldset
+			$("fieldset#get_started").hide();
+			//Show the next fieldset
+			$("fieldset#address").show();
+			//Remove all active states
+			$("ul#tabnav li a").removeClass("tab-active");
+			//Make the next tab active
+			$("ul#tabnav li a#address").addClass("tab-active");
+			
+		}); 
+		
+	
+		$(".next-button").click(function() {
 			//hide the current content
-			$(this).parent().parent(this).hide();
-			//show the next data-tab, aka the next step in registration
-			$(".accordion-content[data-content=" + $(this).attr("data-tab") + "]").show();
+			$(this).parent("fieldset").hide();
+			//show the next content
+			var $fieldsetNext = $(this).parent("fieldset").next("fieldset")
+			$fieldsetNext.show();
 			//make the current tab inactive, by making them all inactive
 			$("ul#tabnav li a").removeClass("tab-active");
 			//make the next tab active
-			$("ul#tabnav li a[data-tab=" + $(this).attr("data-tab") + "]").addClass("tab-active");
-			
+			$("ul#tabnav li a").filter("#" + $fieldsetNext.attr("id")).addClass("tab-active");
 		});
 	
 		$(".prev-button").click(function() {
 			//hide the current content
-			$(this).parent().parent(this).hide();
+			$(this).parent("fieldset").hide();
 			//show the next data-tab, aka the next step in registration
-			$(".accordion-content[data-content=" + $(this).attr("data-tab") + "]").show();	
+			var $fieldsetPrev = $(this).parent("fieldset").prev("fieldset")
+			$fieldsetPrev.show();
 			//make the current tab inactive, by making them all inactive
 			$("ul#tabnav li a").removeClass("tab-active");
 			//make the next tab active
-			$("ul#tabnav li a[data-tab=" + $(this).attr("data-tab") + "]").addClass("tab-active");
+			$("ul#tabnav li a").filter("#" + $fieldsetPrev.attr("id")).addClass("tab-active");
 		});
-
 	});
 }
