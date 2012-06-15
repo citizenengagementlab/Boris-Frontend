ca51e559e48884494694a14e88d693d0693030bb
Jason Webster <jason@metalabdesign.com>
2012-06-14 18:27:47 -0700

    Clean up custom helpers

--- config.rb
+++ config.rb
@@ -9,23 +9,14 @@ helpers do
 
   # Padrino label_tag helper, here to remove stupid trailing colon on the caption.
   def label_tag(name, options={}, &block)
-    # original
-    # options.reverse_merge!(:caption => "#{name.to_s.humanize}: ", :for => name)
-    options.reverse_merge!(:caption => name.to_s.humanize.titleize, :for => name)
-    caption_text = options.delete(:caption)
-    caption_text << "<span class='required'>*</span> " if options.delete(:required)
-    if block_given? # label with inner content
-      label_content = caption_text + capture_html(&block)
-      concat_content(content_tag(:label, label_content, options))
-    else # regular label
-      content_tag(:label, caption_text, options)
-    end
+    options.reverse_merge!(:caption => name.to_s.humanize.titleize, :for => name, :id => name)
+    super
   end
 
   def select_tag(name, options={})
     @select_values ||= YAML.load_file(File.join(settings.root, 'data', 'select_values.yml')).with_indifferent_access
-    options.reverse_merge! :options => @select_values[name]
-    super name, options
+    options.reverse_merge!(:options => @select_values[name]) unless @select_values[name].nil?
+    super
   end
 
 end
