1fff3c8562e9dc9d8c58be25b6107cc828e48965
Josh Levinger <josh.l@engagementlab.org>
2012-06-19 18:27:04 -0700

    encode url params automatically

--- boris/views.py
+++ boris/views.py
@@ -5,21 +5,24 @@ from django.conf import settings
 
 from os.path import join
 import pygeoip
+import urllib
 
 def frontpage(request):
     #lookup IP and redirect user to correct state
     #using free MaxMind GeoLiteCity db
-    geoip = pygeoip.GeoIP(join(settings.STATIC_ROOT,'GeoLiteCity.dat'))
     geoip = pygeoip.GeoIP(join(settings.MEDIA_ROOT,'GeoLiteCity.dat'))
     ip_addr = request.META['REMOTE_ADDR']
+    params = {}
     if settings.DEBUG and ip_addr == "127.0.0.1":
         ip_addr = "75.101.48.104" #test with known CA IP
+        params['debug'] = 1
     result = geoip.record_by_addr(ip_addr)
     if result:
-        params = "?state="+result['region_name']
-        #params += "&zip="+result['postal_code']
+        params['state'] = result['region_name']
+        #params["zip"] = result['postal_code']
         #zip probably not accurate enough to depend on free db
-        return redirect(reverse('registrant.views.register')+params)
+        return redirect(reverse('registrant.views.register')+
+            "?"+urllib.urlencode(params))
     else:
         #should redirect to map view
         #tmp, redirect to registration page w/o params
