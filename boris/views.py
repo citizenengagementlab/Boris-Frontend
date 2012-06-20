f62fab3b84406166ce9c31f2535cad8377571593
Josh Levinger <josh.l@engagementlab.org>
2012-06-19 18:11:17 -0700

    add geoip lookup

--- boris/views.py
+++ boris/views.py
@@ -1,8 +1,28 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response,redirect
+from django.core.urlresolvers import reverse
+from django.conf import settings
+
+from os.path import join
+import pygeoip
 
 def frontpage(request):
-    return redirect('/registrants/new/')
+    #lookup IP and redirect user to correct state
+    #using free MaxMind GeoLiteCity db
+    geoip = pygeoip.GeoIP(join(settings.STATIC_ROOT,'GeoLiteCity.dat'))
+    ip_addr = request.META['REMOTE_ADDR']
+    if settings.DEBUG and ip_addr == "127.0.0.1":
+        ip_addr = "75.101.48.104" #test with known CA IP
+    result = geoip.record_by_addr(ip_addr)
+    if result:
+        params = "?state="+result['region_name']
+        #params += "&zip="+result['postal_code']
+        #zip probably not accurate enough to depend on free db
+        return redirect(reverse('registrant.views.register')+params)
+    else:
+        #should redirect to map view
+        #tmp, redirect to registration page w/o params
+        return redirect(reverse('registrant.views.register'))
 
 def rtv_iframe_test(request):
     layout = request.GET.get('layout')
