761b99b21cf9ab4d58998ffede76a5d19d7751ed
Josh Levinger <josh.l@engagementlab.org>
2012-08-20 16:54:01 -0700

    end test, move map2 to map

--- boris/registrant/urls.py
+++ boris/registrant/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('registrant.views',
     (r'^map/$', 'map'),
-    (r'^map2/$', 'map2'),
     (r'^start/$', 'start'),
     (r'^new/$', 'register'),
     (r'^submit/$', 'submit'),
