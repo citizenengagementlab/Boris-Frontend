cd737f73830f5f364fa9473c86dba0068d9d2226
Josh Levinger <josh.l@engagementlab.org>
2012-09-05 17:50:48 -0700

    show map even when there's no query string

--- boris/media/javascripts/fbint.js
+++ boris/media/javascripts/fbint.js
@@ -247,7 +247,8 @@ window.fbAsyncInit = function() {
          
     //possible TODO - handle cases where user removes authorization with app still open
     FB.Event.subscribe('auth.statusChange', function(response) { statusCheck(response); });
-  } else if (window.location.search.length) {
+  //} else if (window.location.search.length) {
+  } else {
     checkInfo();
     $('#content').css('opacity',1);
   }
