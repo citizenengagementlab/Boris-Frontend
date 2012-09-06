f0be1c35d1b607b5658531840a36fba2f66ae01a
Jordan Pope <jordan@goodmorningstudio.com>
2012-09-05 17:26:09 -0700

    new app name, hiding content until scripts run

--- boris/media/javascripts/fbint.js
+++ boris/media/javascripts/fbint.js
@@ -1,6 +1,6 @@
 var fbAppID = '347566241989371';
-var fbAppUrl = 'https://apps.facebook.com/rtvfbapptest/';
-    
+var fbAppUrl = 'https://apps.facebook.com/rockthevotenow/';
+$('#content').css('opacity',0);
    
 function preAuthView() { //likewise, may want to load a separate pre-auth view, rather than swapping out markup on the fly
   var pA = ['<div id="non-auth">'];
@@ -235,8 +235,10 @@ window.fbAsyncInit = function() {
       'description' : "November will be here before you know it! Are you registered to vote? If not, now's the time! Register here - in 3 easy steps! - using Rock the Vote's online voter registration tool.",
       'media': [{
         "type": "flash", 
-        "swfsrc": "https://s3.amazonaws.com/rocky-boris-test/widgetloader/rtv_fb.swf?v="+(+new Date()), //window.location.origin+"/static/widgetloader/rtv_fb.swf?v="+(+new Date()), 
-        "imgsrc": "https://s3.amazonaws.com/rocky-boris-test/images/flash-preview.gif?v="+(+new Date()), //window.location.origin+"/static/images/flash-preview.gif", 
+        //"swfsrc": "https://s3.amazonaws.com/rocky-boris-test/widgetloader/rtv_fb.swf?v="+(+new Date()), 
+        "swfsrc": window.location.origin+"/static/widgetloader/rtv_fb.swf?v="+(+new Date()), 
+        //"imgsrc": "https://s3.amazonaws.com/rocky-boris-test/images/flash-preview.gif?v="+(+new Date()), 
+        "imgsrc": window.location.origin+"/static/images/flash-preview.gif"+(+new Date()), 
         "width": "130", 
         "height": "87",
         "expanded_width": "398", 
@@ -283,7 +285,8 @@ window.fbAsyncInit = function() {
       
     } else {
       preAuthView();
-    }         
+    }
+    $('#content').css('opacity',1);       
   }
   
   if (getParam("request_ids") != "") {
@@ -296,8 +299,9 @@ window.fbAsyncInit = function() {
          
     //possible TODO - handle cases where user removes authorization with app still open
     FB.Event.subscribe('auth.statusChange', function(response) { statusCheck(response); });
-  } else if (window.location.search.length) {
+  } else if (window.location.search.length) {   
     checkInfo();
+    $('#content').css('opacity',1); 
   }
 };
 
