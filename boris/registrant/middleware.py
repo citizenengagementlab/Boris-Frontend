80fd1ae8c6e338ec6e64a4523174ef95ca23351c
Josh Levinger <josh.l@engagementlab.org>
2012-09-13 10:25:47 -0700

    simplify, because apparently no one uses p3p anyways

--- boris/registrant/middleware.py
+++ boris/registrant/middleware.py
@@ -74,8 +74,8 @@ class MobileDetectionMiddleware(object):
                 redirect_url += "?"+urllib.urlencode(params)
             return HttpResponseRedirect(redirect_url)
 
-
-P3P_COMPACT = "policyref=\"/w3c/p3p.xml\""
+#return p3p headers, so IE doesn't complain about iframe cookies
+P3P_COMPACT = "policyref=\"/w3c/p3p.xml\"" + "CP=\"Rock the Vote has a firm commitment to internet privacy. http://www.rockthevote.com/privacy-policy.html\""
 class MiddlewareResponseInjectP3P(object):
     def __init__(self):
         self.process_response = self.inject
