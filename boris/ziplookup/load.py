8df06c3ab71816fac0b494ee00bdf53028fb1d75
Josh Levinger <josh.l@engagementlab.org>
2012-10-10 11:10:22 -0700

    update loader for new zipcodes format

--- boris/ziplookup/load.py
+++ boris/ziplookup/load.py
@@ -6,12 +6,21 @@ def zips(filename='zipcodes.csv'):
     csv_file = csv.DictReader(open(f_name,'rU'))
     
     i = 1
+    j = 0
     for row in csv_file:
-        z = ZipCode()
-        z.zipcode = row['ZIPCODE']
-        z.city = row['CITY']
-        z.state = row['STATECODE']
+        zipcode = row['ZIPCode']
+        city = row['CityName']
+        state = row['StateAbbr']
+
+        z,new = ZipCode.objects.get_or_create(zipcode=zipcode)
+        z.city = city
+        z.state = state
+
+        if new:
+            j += 1
+            print "update",zipcode
+            z.save()
         
-        z.save()
         i+=1
     print "loaded",i,"zips"
+    print j,"new"
