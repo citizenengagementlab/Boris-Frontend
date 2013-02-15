d064e3087e21266897814be33eb02eec04bdfcef
Josh Levinger <josh.l@engagementlab.org>
2013-02-14 18:12:12 -0800

    use staging aws names

--- boris/settings.py
+++ boris/settings.py
@@ -242,9 +242,9 @@ except:
     #s3 settings
     AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-    AWS_STORAGE_BUCKET_NAME = 'register2.rockthevote.com'
-    AWS_S3_CUSTOM_DOMAIN = 'dyw5n6uc3lgo5.cloudfront.net'
-    STATIC_URL = 'https://dyw5n6uc3lgo5.cloudfront.net/'
+    AWS_STORAGE_BUCKET_NAME = 'rocky-boris-test'
+    AWS_S3_CUSTOM_DOMAIN = 'd3rou2woe8vj33.cloudfront.net'
+    STATIC_URL = 'https://d3rou2woe8vj33.cloudfront.net/'
     STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.CachedStaticFilesStorage'
     #use heroku db
     import dj_database_url
